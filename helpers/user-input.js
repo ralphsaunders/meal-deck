bf6e4c170cc57b6e3b8f21503f1195d7affdd5e7
Ralph Saunders <ralphcgsaunders@gmail.com>
2023-06-09 09:29:43 +0100

    Incomplete user-input helper

---
+++ helpers/user-input.js
@@ -0,0 +1,43 @@
+function detectMeasure(string) {
+    const measures = [
+        'tablespoon',
+        'teaspoon',
+        'tbsp',
+        'tsp',
+        'cup',
+        'c',
+        'fluid ounce',
+        'fl oz',
+        'pint',
+        'pt',
+        'quart',
+        'qt',
+        'gallon',
+        'gal',
+        'milliliter',
+        'ml',
+        'liter',
+        'l',
+        'ounce',
+        'oz',
+        'pound',
+        'lb',
+        '#',
+        'gram',
+        'g',
+        'kilogram',
+        'kg',
+        'pinch',
+        '-',
+        'dash',
+        'drop',
+        'dozen',
+        'doz',
+    ];
+
+    const adjectives = [
+        'scant',
+        'generous',
+        'heaped'
+    ];
+}
