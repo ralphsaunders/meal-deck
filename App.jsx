9fc165908032b822f64177f686dcddc6f08d798e
Ralph Saunders <ralphcgsaunders@gmail.com>
2023-06-11 18:06:03 +0100

    Stub out "New Shop" screen

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-
+import React, { useState } from "react";
+import { TouchableOpacity, Text, Modal, View, StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
@@ -7,6 +7,7 @@ import { MealsScreen } from "./app/components/MealsScreen";
 import { MealDetailScreen } from "./app/components/MealDetailScreen";
 import { ShoppingListsScreen } from "./app/components/ShoppingListsScreen";
 import { ShoppingListDetailScreen } from "./app/components/ShoppingListDetailScreen";
+import { NewShopScreen } from "./app/components/NewShopScreen";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -33,34 +34,121 @@ const ShoppingListsStack = () => {
     );
 };
 
+const CustomTabBarButton = ({ children, onPress }) => (
+    <TouchableOpacity onPress={onPress} style={{ flex: 1 }}>
+        {children}
+        <Text style={{ textAlign: "center" }}>+</Text>
+    </TouchableOpacity>
+);
+
 /**
  * Root App component
  * @returns {string} App component tree
  */
 export default function App() {
+    const [newShopModalVisible, setNewShopModalVisible] = useState(false);
+
+    const handleNewShopPress = () => {
+        setNewShopModalVisible(true);
+    };
+    const handleModalClose = () => {
+        setNewShopModalVisible(false);
+    };
+
     return (
-        <NavigationContainer>
-            <Tab.Navigator
-                initialRouteName="MealsTab"
-                screenOptions={{
-                    headerShown: false,
-                }}
-            >
-                <Tab.Screen
-                    name="MealsTab"
-                    component={MealsStack}
-                    options={{
-                        tabBarLabel: "Meals",
+        <View style={styles.container}>
+            <NavigationContainer>
+                <Tab.Navigator
+                    initialRouteName="MealsTab"
+                    screenOptions={{
+                        headerShown: false,
                     }}
-                />
-                <Tab.Screen
-                    name="ShoppingListTab"
-                    component={ShoppingListsStack}
-                    options={{
-                        tabBarLabel: "Shopping Lists",
-                    }}
-                />
-            </Tab.Navigator>
-        </NavigationContainer>
+                >
+                    <Tab.Screen
+                        name="MealsTab"
+                        component={MealsStack}
+                        options={{
+                            tabBarLabel: "Meals",
+                        }}
+                    />
+                    <Tab.Screen
+                        name="ShoppingListTab"
+                        component={ShoppingListsStack}
+                        options={{
+                            tabBarLabel: "Shopping Lists",
+                        }}
+                    />
+                    <Tab.Screen
+                        name="New Shop"
+                        component={NewShopScreen}
+                        options={{
+                            tabBarButton: (props) => (
+                                <CustomTabBarButton
+                                    {...props}
+                                    onPress={handleNewShopPress}
+                                />
+                            ),
+                        }}
+                    />
+                </Tab.Navigator>
+                <Modal
+                    visible={newShopModalVisible}
+                    animationType="slide"
+                    presentationStyle="pageSheet"
+                >
+                    <View style={styles.modalContainer}>
+                        <View style={styles.modalContent}>
+                            <TouchableOpacity
+                                onPress={handleModalClose}
+                                style={styles.modalCloseButton}
+                            >
+                                <Text style={styles.modalCloseButtonText}>
+                                    Close
+                                </Text>
+                            </TouchableOpacity>
+                            <Text style={styles.modalTitle}>New Shop</Text>
+                            <Text>Modal content goes here</Text>
+                        </View>
+                    </View>
+                </Modal>
+            </NavigationContainer>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    tabBarButton: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    tabBarButtonText: {
+        fontSize: 24,
+    },
+    modalContent: {
+        backgroundColor: "white",
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        paddingTop: 20,
+        paddingHorizontal: 20,
+        paddingBottom: 40,
+    },
+    modalTitle: {
+        fontSize: 20,
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+    modalCloseButton: {
+        alignSelf: "flex-start",
+        paddingVertical: 8,
+        paddingHorizontal: 0,
+        marginBottom: 10,
+    },
+    modalCloseButtonText: {
+        fontSize: 16,
+        color: "#007AFF", // iOS blue color
+    },
+});
---
+++ app/components/NewShopScreen.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+export function NewShopScreen() {
+    return (
+        <View>
+            <Text>New Shop Screen</Text>
+        </View>
+    );
+}
